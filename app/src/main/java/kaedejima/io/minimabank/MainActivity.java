a1a74c8fc7c8ec6542ff369d66fa9c36e5ad0468
飯島楓 <kaedei@YEL-PC.local>
2019-04-27 13:24:36 +0900

    init

---
+++ app/src/main/java/kaedejima/io/minimabank/MainActivity.java
@@ -0,0 +1,13 @@
+package kaedejima.io.minimabank;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @   Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
